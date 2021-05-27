57be442beaa6672a333e7af8d34bffeb5c9f9dd4
Saksham <saxamdhyani1999@gmail.com>
2021-05-27 20:22:37 +0530

    todo complete

--- 6_browser_todo_23_04_2021/activity/new/script.js
+++ 6_browser_todo_23_04_2021/activity/new/script.js
@@ -1,13 +1,21 @@
 let colorbtn = document.querySelectorAll(".filter_colorbox");
 let body = document.body;
+let modal_container = document.querySelector(".modal_container");
+let show = false;
+let showhide = document.querySelector(".showHide");
+let enterButton = document.querySelector(".enterButton");
+let textArea = document.querySelector(".text_area");
 let cColor = "color4";
+let colors = ["color1", "color2", "color3", "color4"];
+let info = document.querySelector(".info");
 let task = "";
 let taskArr = [];
 let bothElementsArr = document.querySelectorAll(".icon-container");
 let crossBtn = bothElementsArr[1];
 let plusButton = bothElementsArr[0];
 let deleteState = false;
-let mainContainer = document.querySelector(".main_container")
+let mainContainer = document.querySelector(".main_container");
+let trashBtn = document.querySelector(".trash");
 if (localStorage.getItem("allTask")) {
     taskArr = JSON.parse(localStorage.getItem("allTask"));
     // UI
@@ -16,38 +24,86 @@ if (localStorage.getItem("allTask")) {
         createTask(color, task, false, id);
     }
 }
+updateInfo();
+
+modal_container.addEventListener("click", function() {
+    console.log("heel")
+        // showhide.classList.remove("show");
+        // showhide.classList.add("hide");
+})
+mainContainer.addEventListener("click", function() {
+    mainContainer.style.opacity = "1";
+    if (showhide.classList[1] == "show") {
+        showhide.classList.remove("show");
+        showhide.classList.add("hide");
+    }
+
+
+})
+
+function updateInfo() {
+    info.innerText = `you have ${taskArr.length} task left`;
+}
+
+trashBtn.addEventListener("click", function() {
+    if (taskArr.length > 0) {
+        if (confirm("Are you sure...?")) {
+            taskArr = [];
+            localStorage.setItem("allTask", JSON.stringify(taskArr));
+            updateInfo();
+            let allTickets = mainContainer.childNodes;
+            for (let i = allTickets.length - 1; i > 0; i--) {
+                allTickets[i].remove();
+            }
+        }
+    }
+})
+
+// event listeners on filter button
+//  tickets with that color will be visible
+for (let i = 0; i < colorbtn.length; i++) {
+    colorbtn[i].addEventListener("click", function() {
+        let selectedColor = colorbtn[i].classList[0];
+        console.log(selectedColor);
+        let allTasks = mainContainer.querySelectorAll(".task_filter");
+        // console.log(allTasks);
+        for (let j = 0; j < allTasks.length; j++) {
+            // if (allTasks[j].childNodes[0].classList.add("hide"));
+
+            if (selectedColor == "white") {
+                allTasks[j].parentNode.style.display = "block";
+
+            } else if (allTasks[j].classList[1] != selectedColor) {
+                // console.log(j);
+                allTasks[j].parentNode.style.display = "none";
+            } else {
+
+                allTasks[j].parentNode.style.display = "block";
+            }
+
+        }
+    })
+}
+
+
+
+
+// console.log(taskArr);
+
+//  plus button listener
 plusButton.addEventListener("click", openmodale);
-crossBtn.addEventListener("click", setDeleteState)
+
+
 
 function openmodale() {
-    let modal_container = document.createElement("div");
-    modal_container.setAttribute("class", "modal_container");
+    showhide.classList.remove("hide");
+    showhide.classList.add("show");
+    mainContainer.style.opacity = "0.3";
 
-    let modalcontainercode = ` 
-    <div class="task_message_panel">
-        <textarea name="textarea" class=text_area id="" cols="30" rows="10" placeholder="Enter your task here"></textarea>
-        <!-- <input type="text" name="text" placeholder="Enter your task here" class="text_area"> -->
-    </div>
-    <div class="colour_select_panel">
-        <div class="colour_option">
-            <div class="colour_select color1"></div>
-        </div>
-        <div class="colour_option">
-            <div class="colour_select color2"></div>
-        </div>
-        <div class="colour_option">
-            <div class="colour_select color3"></div>
-        </div>
-        <div class="colour_option">
-            <div class="colour_select color4"></div>
-        </div>
-    
-    </div>`;
-    modal_container.innerHTML = modalcontainercode;
-    body.appendChild(modal_container);
     let modal_color = document.querySelectorAll(".colour_select");
     //initial selected color
     modal_color[3].classList.add("selected_border");
+
 
     //Events management for color boxes in modale
     for (let i = 0; i < modal_color.length; i++) {
@@ -63,19 +119,35 @@ function openmodale() {
         });
     }
     //textArea selectn and event management
-    let textArea = document.querySelector(".text_area");
     textArea.addEventListener("keydown", (e) => {
         if (e.key == "Enter" && textArea.value != "") {
             task = textArea.value;
             textArea.value = "";
             console.log(task + "----" + cColor);
-            modal_container.remove();
+            showhide.classList.remove("show");
+            showhide.classList.add("hide");
+            mainContainer.style.opacity = "1";
+
             createTask(cColor, task, true);
         }
     })
 
 
 }
+enterButton.addEventListener("click", function() {
+    if (textArea.val != "") {
+        task = textArea.value;
+        textArea.value = "";
+        console.log(task + "----" + cColor);
+        showhide.classList.remove("show");
+        showhide.classList.add("hide");
+        mainContainer.style.opacity = "1";
+
+        createTask(cColor, task, true);
+    }
+
+})
+
 
 function createTask(color, task, flag, id) {
     // color area click-> among colors
@@ -86,10 +158,10 @@ function createTask(color, task, flag, id) {
     taskContainer.setAttribute("class", "task_container");
     taskContainer.innerHTML = `<div class="task_filter ${color}"></div>
     <div class="task_desc_container">
-        <h3 class="uid">#${uid}</h3>
-        <div class="task_desc" contenteditable="true" >${task}</div>
+    <h3 class="uid">#${uid}</h3>
+    <div class="task_desc" contenteditable="true" >${task}</div>
     </div>
-</div >`;
+    </div >`;
     mainContainer.appendChild(taskContainer);
     let taskFilter = taskContainer.querySelector(".task_filter");
     if (flag == true) {
@@ -103,12 +175,12 @@ function createTask(color, task, flag, id) {
     taskContainer.addEventListener("click", deleteTask);
     let taskDesc = taskContainer.querySelector(".task_desc");
     taskDesc.addEventListener("keypress", editTask);
+    updateInfo();
 
 }
 
 function changeColor(e) {
 
-    let colors = ["color1", "color2", "color3", "color4"];
     let taskFilter = e.currentTarget;
     let cColor = taskFilter.classList[1];
     let idx = colors.indexOf(cColor);
@@ -131,6 +203,12 @@ function changeColor(e) {
     }
 
 }
+
+//   delete button listener
+crossBtn.addEventListener("click", setDeleteState);
+
+
+// alter delete State
 
 function setDeleteState(e) {
     let crossBtn = e.currentTarget;
@@ -161,9 +239,12 @@ function deleteTask(e) {
                 break;
             }
         }
+        updateInfo();
     }
 }
 
+
+// update task in local Storage ..... Local Storage
 function editTask(e) {
     let taskDesc = e.currentTarget;
     let uidElem = taskDesc.parentNode.children[0];
